47f5eb4c0d35cd028944d938cef09f1b3e6d5333
itsparasbisht <iamparasbisht@gmail.com>
2023-06-08 21:03:21 +0530

    implementing the useTransition hook

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import { useState, useTransition } from "react";
+
 function About() {
+  const [isPending, startTransition] = useTransition();
+  const [name, setName] = useState("");
+  const [list, setList] = useState<string[]>([]);
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setName(e.target.value);
+    startTransition(() => {
+      const l = [];
+      for (let i = 0; i < 5000; i++) {
+        l.push(e.target.value);
+      }
+      setList(l);
+    });
+  }
+
   return (
     <div>
-      <h1>This is the about page</h1>
+      <input type="text" value={name} onChange={handleChange} />
+      {isPending
+        ? "loading"
+        : list.map((item, index) => <p key={index}>{item}</p>)}
     </div>
   );
 }
